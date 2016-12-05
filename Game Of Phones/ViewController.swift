ea9c3e1b059499de56ce494c4ff1b9e6dad17ff7
Josh Harrold <joshharrold1@joshs-macbook-pro.local>
2016-12-05 00:03:03 -0600

    Initial Commit

---
+++ Game Of Phones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game Of Phones
+//
+//  Created by Josh Harrold on 12/4/16.
+//  Copyright © 2016 Josh Harrold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
