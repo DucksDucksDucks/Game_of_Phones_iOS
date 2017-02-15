9022a1451f0b96baaff0b82d085070564cbf759a
Josh Harrold <joshharrold1@joshs-mbp.lan>
2017-02-15 13:44:01 -0600

    Add ability to retrieve and parse device id from son array

--- Game Of Phones/ViewController.swift
+++ Game Of Phones/ViewController.swift
@@ -24,10 +24,13 @@ class ViewController: UIViewController {
         request.httpMethod = "POST"
         request.httpBody = postString.data(using: .utf8)
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+            guard let data = data, error == nil else {
+                // check for fundamental networking error
                 print("error=\(error)")
                 return
             }
+            
+            self.getDeviceId(data: data)
             
             if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
@@ -39,9 +42,21 @@ class ViewController: UIViewController {
         }
         task.resume()
         
-        
-        
-        
+    }
+    
+    func getDeviceId(data: Data) -> Any{
+        var deviceId = ""
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
+            if let deviceIdArray = json["deviceID"] as? [[String:AnyObject]]{
+                deviceId = deviceIdArray[0]["device_id"] as! String
+                print(deviceId)
+            }
+        }
+        catch let error as NSError {
+            print(error)
+        }
+        return deviceId
     }
     
     
