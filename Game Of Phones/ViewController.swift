f96476102cc24c9c8ff64214af73a309f36ce55d
Josh Harrold <joshharrold1@joshs-mbp.lan>
2017-02-15 16:33:38 -0600

    Device ID is now being sent to the web server correctly

--- Game Of Phones/ViewController.swift
+++ Game Of Phones/ViewController.swift
@@ -30,7 +30,43 @@ class ViewController: UIViewController {
                 return
             }
             
-            self.getDeviceId(data: data)
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+            
+            self.postDeviceId(data: data)
+        }
+        task.resume()
+        
+    }
+    
+    func postDeviceId(data: Data){
+        var deviceId = ""
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
+            if let deviceIdArray = json["deviceID"] as? [[String:AnyObject]]{
+                deviceId = deviceIdArray[0]["device_id"] as! String
+                //print(deviceId)
+            }
+        }
+        catch let error as NSError {
+            print(error)
+        }
+        var request = URLRequest(url: URL(string: "http://mcs.drury.edu/amerritt/isTeacherIDSet.php")!)
+        let deviceIdData = "deviceID=\(deviceId)"
+        print(deviceIdData)
+        request.httpMethod = "POST"
+        request.httpBody = deviceIdData.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
             
             if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
@@ -41,24 +77,7 @@ class ViewController: UIViewController {
             print("responseString = \(responseString)")
         }
         task.resume()
-        
     }
-    
-    func getDeviceId(data: Data) -> Any{
-        var deviceId = ""
-        do {
-            let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
-            if let deviceIdArray = json["deviceID"] as? [[String:AnyObject]]{
-                deviceId = deviceIdArray[0]["device_id"] as! String
-                print(deviceId)
-            }
-        }
-        catch let error as NSError {
-            print(error)
-        }
-        return deviceId
-    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
