9ad7b5427ef5259023b58204e8dbd1dc74779a4b
Joshua Harrold <jharrold@gamed25nv0mtfy14.drury.edu>
2017-02-17 09:51:26 -0600

    Checks if teacher id is set in database

--- Game Of Phones/SecondViewController.swift
+++ Game Of Phones/SecondViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var teacherId: UITextField!
+    
+    
+    @IBAction func submitButton(_ sender: UIButton) {
+        
+        let postString = "deviceID=\(teacherId.text!)"
+        
+        
+        var request = URLRequest(url: URL(string: "http://mcs.drury.edu/amerritt/isTeacherIDSet.php")!)
+        request.httpMethod = "POST"
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+            
+        }
+        task.resume()
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
