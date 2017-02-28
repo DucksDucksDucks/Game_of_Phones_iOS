8cae1e16b15cbd98b090a59bbf04dfa39a231fa8
Josh Harrold <joshharrold1@joshs-mbp.lan>
2017-02-27 21:16:09 -0600

    Renamed the view controller following answer submission

--- Game Of Phones/AnswerSubmittedViewController.swift
+++ Game Of Phones/AnswerSubmittedViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FourthViewController: UIViewController {
+class AnswerSubmittedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Game Of Phones/NicknameViewController.swift
+++ Game Of Phones/NicknameViewController.swift
@@ -61,12 +61,10 @@ class NicknameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
